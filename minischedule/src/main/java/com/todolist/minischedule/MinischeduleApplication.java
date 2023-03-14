231d86a987563fb13f47c031aac31f4f00f44eee
Pedro Henrique <pedro.pereira@visto.global>
2023-03-14 01:25:50 -0300

    Back end

---
+++ minischedule/src/main/java/com/todolist/minischedule/MinischeduleApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.minischedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinischeduleApplication {
+
+		public static void main(String[] args) {
+		SpringApplication.run(MinischeduleApplication.class, args);
+	}
+
+}
---
+++ minischedule/src/test/java/com/todolist/minischedule/MinischeduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.minischedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinischeduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
